bdaa472d15634d82cc9fcf47c2eaa0430eaff14d
sanibr <sanibr89@gmail.com>
2021-03-14 15:19:56 +0530

    test

--- update.php
+++ update.php
@@ -6,8 +6,8 @@ include_once("class/Report.php");
 
 $crud = new Crud();
 
-if(isset($_POST["id"]))
-{
+if(isset($_POST["id"])){
+	
  $value = $crud->escape_string($_POST["value"]);
  $query = "UPDATE sales_master SET ".$_POST["column_name"]."='".$value."' WHERE Sales_Master_Id = '".$_POST["id"]."'";
  $crud->execute($query);
